829e6e8d5bb8ea9cd37263c81089bb9db81a5d8d
hussain <hussain@gmail.com>
2025-12-25 08:47:20 +0500

    first commit

---
+++ base/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/products/',
+        'api/products/<id>/',
+
+    ]
+    return Response(routes)
