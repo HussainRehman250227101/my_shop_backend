e0754a80b443c01a5f2013b3b78c4d2d43883ebe
hussain <hussain@gmail.com>
2025-12-25 10:44:27 +0500

    api made

--- backend/settings.py
+++ backend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     
     
     'rest_framework',
-
+    "corsheaders",
     'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS =True
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .products import products
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -10,3 +11,18 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product = None 
+    for i in products:
+        if i['_id'] == pk :
+            product = i
+            break
+    
+    return Response(product)
+
