2c69e692257e311b5bf1e31279a89b575eae0054
hussain <hussain@gmail.com>
2025-12-25 13:42:27 +0500

    all models added

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product, Review ,Order
+from .models import Product, Review ,Order,ShippingAddress,OrderItem
 
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Order)
+admin.site.register(ShippingAddress)
+admin.site.register(OrderItem)
--- base/models.py
+++ base/models.py
@@ -47,3 +47,27 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.createdAt) 
+
+class ShippingAddress(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    order = models.OneToOneField(Order,on_delete=models.CASCADE,null=True,blank=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=200, null=True,blank=True)
+    postalCode = models.IntegerField(default=0, null=True,blank=True)
+    country = models.CharField(max_length=200, null=True,blank=True)
+    shippingPrice = models.DecimalField(max_digits=10, decimal_places=2 ,null=True, blank=True ) 
+
+    def __str__(self):
+        return str(self.order) 
+    
+class OrderItem(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,null=True, blank = True)
+    product =models.ForeignKey(Product,on_delete=models.CASCADE,null=True, blank = True)
+    name = models.CharField(max_length=200, null=True,blank=True)
+    qty = models.IntegerField(default=0, null=True,blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2 ,null=True, blank=True )
+    image = models.CharField(max_length=200, null=True,blank=True)
+
+    def __str__(self):
+        return str(f"{self.order} ------ {self.name} ")
